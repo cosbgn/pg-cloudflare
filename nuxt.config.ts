afe7133d11937dd6dd6060f40c413e4761cd38d7
Costantin Bignoli <cosbgn@gmail.com>
2024-10-05 13:44:39 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-09-25",
+	future: { compatibilityVersion : 4 },
 	nitro: {
 		rollupConfig: {
 			external: ["cloudflare:sockets"],
