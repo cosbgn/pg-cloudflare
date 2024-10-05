bc54155addfa91ca617acc2774e85e53ac50969e
Costantin Bignoli <cosbgn@gmail.com>
2024-10-05 14:09:07 +0300

    added extrenals back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-09-25",
 	future: { compatibilityVersion : 4 },
+	nitro: {
+		rollupConfig: {
+			external: ["cloudflare:sockets"],
+		}
+	},
 	devtools: { enabled: false }
 })
